1273f489bb368ec860739cd28d845d8d1123acb4
LeiscarT <leiscartp29@gmail.com>
2021-06-05 15:14:40 -0400

    Add transform object

--- src/Wrapper.scala
+++ src/Wrapper.scala
@@ -2,7 +2,26 @@
 
 object Wrapper {
   
-  def main (Args: Array[String]){
+  def transform (text: String, column: Int) : String = 
+  {
+    if (text.length() <= column){
+      return text
+    }
     
+    val wrapper = text.substring(0,column) + "\n"
+    val remains = text.substring(column)
+    
+    return wrapper + transform(remains, column) 
+    
+    
+  }
+  
+  
+  
+  def main (Args: Array[String]){
+    println(transform("hola",2))
+     println(transform("hola",3))
+      println(transform("hosdasdasdla",6))
+     
   }
 }
